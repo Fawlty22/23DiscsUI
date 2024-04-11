a92ab137d7eb69973a895e2d58ff5c5df297b0e4
Matt Keys <MatthewjKeys22@gmail.com>
2024-04-11 16:09:07 -0400

    hide password on login screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,16 +3,18 @@ import { ReactiveFormsModule, FormGroup, Validators, FormBuilder} from '@angular
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { AuthService } from '../core/services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  hidePassword: boolean = false;
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
@@ -22,8 +24,8 @@ export class LoginComponent {
 
 
   onSubmit() {
-      // console.log(this.loginForm.value);
-      this.authService.login(this.loginForm.value)
+    console.log('submit')
+      this.authService.login(this.loginForm.value);
   }
 
 }
