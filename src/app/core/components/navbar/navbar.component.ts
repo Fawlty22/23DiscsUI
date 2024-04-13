cc207977b69435fa531e410f41ce3987098d14b0
Matt Keys <MatthewjKeys22@gmail.com>
2024-04-13 17:41:46 -0400

    favicon, navbar updates

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -6,10 +6,11 @@ import {MatMenuModule} from '@angular/material/menu';
 import { AuthService } from '../../services/auth.service';
 import { RouterModule } from '@angular/router';
 import { DiscService } from '../../services/disc.service';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, RouterModule],
+  imports: [ MatToolbarModule, MatTooltipModule, MatIconModule, MatButtonModule, MatMenuModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
