914993fd651f4b71ce0e54266df965130032e96c
Matt Keys <MatthewjKeys22@gmail.com>
2024-05-06 20:19:04 -0400

    dev api url change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    url: 'http://ec2-44-201-179-25.compute-1.amazonaws.com:3000'
+    url: 'http://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
 };
 
 // url: 'http://localhost:3000'
