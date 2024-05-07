e9e22bf2661a0a596b17c87b47b24696a16ba8de
Matt Keys <MatthewjKeys22@gmail.com>
2024-05-06 20:49:57 -0400

    enviroment switch

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: true,
-    url: 'http://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
+    production: false,
+    url: 'http://localhost:3000'
 };
 
-// url: 'http://localhost:3000'
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    url: 'http://localhost:3000'
+    production: true,
+    url: 'http://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
 };
