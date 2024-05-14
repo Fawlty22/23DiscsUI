1978b3377d5778e4bc785f905ad8db16623e9fb7
Matt Keys <MatthewjKeys22@gmail.com>
2024-05-14 12:45:09 -0400

    https for ec2 url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    url: 'http://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
+    url: 'https://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
 };
