d4e8b5682c4e0ba600ead8080cb09560e50a68a3
Matt Keys <MatthewjKeys22@gmail.com>
2024-05-15 17:06:38 -0400

    api.url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    url: 'https://23DiscsLoadBalancer-2024885548.us-east-1.elb.amazonaws.com'
+    url: 'https://www.api.22discs.com'
+    // url: 'https://23DiscsLoadBalancer-2024885548.us-east-1.elb.amazonaws.com'
     // url: 'https://ec2-54-90-78-124.compute-1.amazonaws.com:3000'
 };
